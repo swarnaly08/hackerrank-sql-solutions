0780be000f0bb2252f95c034e876c5b2fae4a8da
Swarnaly Roy <swarnalyroy667@gmail.com>
2026-03-26 15:53:09 +0530

    Add files via upload

---
+++ Occupations.sql
@@ -0,0 +1,14 @@
+SELECT
+    MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
+    MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
+    MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
+    MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
+FROM (
+    SELECT
+        Name,
+        Occupation,
+        ROW_NUMBER() OVER(PARTITION BY Occupation ORDER BY Name) AS rn
+    FROM Occupations
+) AS Subquery
+GROUP BY rn
+ORDER BY rn;
